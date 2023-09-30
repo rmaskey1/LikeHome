94cd48ebdd6b1652361ac8dceea58db072f6c917
Hai <hai.ly@sjsu.edu>
2023-09-30 11:41:14 -0700

    Firestore Python Database

--- Backend/server.py
+++ Backend/server.py
@@ -1,5 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials
-
+from firebase_admin import firestore
 cred = credentials.Certificate("C:\\Users\\haily\\Documents\\serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+
+# Application Default credentials are automatically created.
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+# Create Collection 
+doc_ref = db.collection("users").document("alovelace")
+doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
