bd93985a1da2c05e4540329463221c5ffd96d1f4
rmaskey1 <maskey.reshaj@gmail.com>
2023-10-08 18:42:05 -0700

    Finalized hotel and guest signup
    - Added access rights for hotel and guest user types
    - Created 'user' database for custom fields and general use (separate from authentication database)
    - Added frontend elements to test out function

--- Backend/database.py
+++ Backend/database.py
@@ -9,21 +9,65 @@ from flask import Flask, request, jsonify
 # 4. Drag that file and add it into backend folder if you have not done so
 # 5. Make sure you are in cmpe165-likehome directory and not Backend directory
 # 6. Run the python file
-cred = credentials.Certificate("./serviceAccountKey.json")
+cred = credentials.Certificate("./Backend/serviceAccountKey.json")
 
 # Application Default credentials are automatically created.
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 # Function to add user to database
-def addUser(email, phone, password, dispName):
+def addUser(email, phone, password, firstName, lastName, type, hotel=None): # 'hotel' is an OPTIONAL parameter, only applies for users registering as a hotel
+    dispName = firstName + lastName
+    # Adding user to authentication database
     user = auth.create_user(
-    email=email,
-    phone_number=phone,
-    password=password,
-    display_name=dispName)
+        email=email,
+        phone_number=phone,
+        password=password,
+        display_name=dispName
+    )
+    # Setting access rights for added user
+    if type == "guest":
+        auth.set_custom_user_claims(user.uid, {
+            'guest': True,
+            'hotel': False
+        })
+    elif type == "hotel":
+        auth.set_custom_user_claims(user.uid, {
+            'guest': False,
+            'hotel': True
+        })
+    
+    # Adding user to custom "user" database
+    if hotel == None:
+        doc_ref = db.collection("user").add({
+            'firstName': firstName,
+            'lastName': lastName,
+            'email': email,
+            'phone': phone,
+            'password': password,
+            'accountType': type
+        })
+    else:
+        doc_ref = db.collection("user").add({
+            'firstName': firstName,
+            'lastName': lastName,
+            'email': email,
+            'phone': phone,
+            'password': password,
+            'hotelName': hotel,
+            'accountType': type
+        })
     print('Sucessfully created new user: {0}'.format(user.uid))
 
 # Main method for testing
 def main():
-    addUser("email@email.com", "+15555555555", "password", "Mike mike")
+    # addUser("gmail@email.com", "+15555555557", "password", "Mike", "Mike", "guest")
+    # user1 = auth.get_user_by_email("email@email.com")
+    # if user1.custom_claims.get('guest') == True:
+    #     print("works")
+    
+    addUser("qmail@email.com", "+15678555557", "password", "Mike", "Mike", "hotel", "hotel inn")
+    user2 = auth.get_user_by_email("email@email.com")
+    if user2.custom_claims.get('guest') == True:
+        print("works")
+# main()
