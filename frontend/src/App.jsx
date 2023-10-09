fa529fe9bde52837f60de2324c6ac92a5965e05c
huynai <116304319+huynai@users.noreply.github.com>
2023-10-09 16:26:27 -0700

    add component to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,9 @@ import Login from "routes/Login";
 import NotFound from "routes/NotFound";
 import Profile from "routes/Profile";
 import Register from "routes/Register";
+import AddListing from "routes/AddListing";
+import AccountMod from "routes/AccountMod";
+import PreviewCardsListing from "components/PreviewCardsListing";
 
 function App() {
   return (
@@ -17,6 +20,12 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/hotel/:id" element={<Details />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/add_listing" element={<AddListing />} />
+        <Route path="/hotel/:id/modify_listing" element={<AddListing />} />
+        <Route path="/account_modification" element={<AccountMod />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/home" element={<PreviewCardsListing />} />
       </Routes>
     </BrowserRouter>
   );
