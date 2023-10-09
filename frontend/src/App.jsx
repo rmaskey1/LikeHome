c1a821fabed95d4c57401760680cc9e76f952421
Jooyul Yoon <juyeolyoon@gmail.com>
2023-10-08 23:54:17 -0700

    Create Profile Page and NotFound Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Details from "routes/Details";
 import Home from "routes/Home";
 import Login from "routes/Login";
+import NotFound from "routes/NotFound";
+import Profile from "routes/Profile";
 import Register from "routes/Register";
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/hotel/:id" element={<Details />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
