c22dd725d89fba346b3ff361fa820a968536da00
Baotran <baotran.nguyen@sjsu.edu>
2023-10-12 16:11:01 -0700

    Fixing

--- Backend/listing.py
+++ Backend/listing.py
@@ -30,7 +30,7 @@ def listing_modification_func(app):
         if roomBooked() == False:
             #cYXBww5bSw4nYbdv2RzM
             if is_start_date_before_or_on_end_date(data['fromDate'], data['toDate']):
-                update_room(data['rid'], data['price'], format_date(data['fromDate']), format_date(data['toDate']), data['beds'], data['guests'], data['bathrooms'], data['bedType'], data['image'], amenities)
+                update_room(request.args['rid'], data['price'], format_date(data['fromDate']), format_date(data['toDate']), data['beds'], data['guests'], data['bathrooms'], data['bedType'], data['image'], amenities)
                 return jsonify({'message': 'Listing modification was successful'})
             else:
                 abort(make_response(jsonify(message="Start date cannot be after end date"), 400))
