d4cc95605b34541eb9086982ff0b66f11e66bd49
Isabel Luong <isabel.luong@sjsu.edu>
2023-10-28 06:39:14 -0700

    merge

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -192,6 +192,7 @@ function Home() {
           </Mylisting>
           {myListingsIsLoading ? (
             <LoadingCardsListing numCard={1} />
+            <LoadingCardsListing numCard={1} />
           ) : (
             <PreviewCardsListing listings={myListings} />
           )}
@@ -229,6 +230,7 @@ function Home() {
       </div>
 
       {allListingsIsLoading ? (
+        <LoadingCardsListing numCard={20} />
         <LoadingCardsListing numCard={20} />
       ) : (
         <PreviewCardsListing
